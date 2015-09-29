8b40c4e2065bd31901d73a12f57b5a6a058105f6
p-guerin <guerin.pierre26@gmail.com>
2015-09-29 13:35:12 +0200

    Refactoring questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,14 +1,9 @@
 class QuestionsController < ApplicationController
 	def new_question
-		@next_question = next_question_int.to_s
-		if next_question_int > nb_question 
-			render 'result'
-		else
-			next_question_int == nb_question ? (@next_question_text = "Fin") : (@next_question_text = "Question suivante")
-			@question = Question.find(params[:id])
-			@answers = Answer.where(id_question: params[:id]).take
-			render 'question'
-		end
+		end? ? (@next = "/result") : (@next = next_question_int.to_s)
+		@question = Question.find(params[:id])
+		@answers = Answer.where(id_question: params[:id]).take
+		render 'question'
 	end
 
 	def next_question_int
@@ -19,5 +14,8 @@ class QuestionsController < ApplicationController
 		Question.all.length + 1
 	end
 
-	
+	def end?
+		next_question_int >= nb_question
+	end
+
 end
