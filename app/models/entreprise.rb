e7b5397154c504f9321ae1ebd86d31693af60552
parmeso6 <sophie.marie.parmentier@gmail.com>
2015-09-29 09:38:55 +0200

    reset gen curl

--- app/models/entreprise.rb
+++ app/models/entreprise.rb
@@ -1,12 +1,3 @@
-require 'curl'
-
 class Entreprise < ActiveRecord::Base
-	def naf_translated()
-		curl = CURL.new
-		adresse = "http://recherche-naf.insee.fr/SIRENET_ClassesNaf/"+self.naf+".htm"
-		page = curl.get(adresse)
-		page_array = page.split("title")
-		page_sous = page_array[1].split("Sous-classe - ")
-		page_sous[1]
-	end
+
 end
