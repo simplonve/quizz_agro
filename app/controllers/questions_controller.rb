7d7c2ba9b2751d9ea66858dbf8800b38f3a44497
p-guerin <guerin.pierre26@gmail.com>
2015-09-30 06:54:30 +0200

    Refactoring du controller questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,8 @@
 class QuestionsController < ApplicationController
 	def new_question
-		end? ? (@next = "/result") : (@next = next_question_int.to_s)
 		@question = Question.find(params[:id])
-		@answers = Answer.where(id_question: params[:id]).take
-		@arr_answers = []
-		@arr_answers.push(@answers.good_answer, @answers.bad_answers).shuffle!
+		answers
+		next_link
 		render 'question'
 	end
 
@@ -16,8 +14,21 @@ class QuestionsController < ApplicationController
 		Question.all.length + 1
 	end
 
+	def obj_answer
+		@answers = Answer.where(id_question: params[:id]).take
+	end
+
+	def answers
+		@arr_answers = []
+		@arr_answers.push(obj_answer.good_answer, obj_answer.bad_answers).shuffle!
+	end
+
 	def end?
 		next_question_int >= nb_question
+	end
+
+	def next_link
+		end? ? (@next = "/result") : (@next = next_question_int.to_s)
 	end
 
 end
