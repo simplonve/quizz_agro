3639056ae827d53ebff1858e32177719b713f1bf
Vincent Jumeaux <vincent.jumeaux@gmail.com>
2015-10-02 15:25:57 +0200

    add test on question and some fixtures

---
+++ test/controllers/questions_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class QuestionsControllerTest < ActionController::TestCase
+  test "question" do
+    question = Question.first
+
+    get :question, id: question.id
+    assert :success
+    assert assigns(:good_answer)
+    assert assigns(:interro)
+    assert_equal 4, assigns(:choices).length
+  end
+end
+
